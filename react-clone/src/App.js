cec894ec2465f2c3cc2445eb482ae2378a5d97a0
kojesung <js95112345@gmail.com>
2023-12-29 16:03:53 +0900

    useEffect 활용

--- react-clone/src/App.js
+++ react-clone/src/App.js
@@ -1,20 +1,38 @@
-import Button from "./Button";
+/*import Button from "./Button";*/
 import PropTypes from "prop-types";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 function App() {
+  const [keyword, setKeyword] = useState('');
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => (setKeyword(event.target.value));
+  console.log("I run all the time")
+  useEffect(() => {
+    console.log("CALL THE API")
+  }, [])
+  useEffect(() => {
+    console.log("Search for", keyword)
+  }, [keyword])
+  useEffect(() => {
+    console.log("I run keyword and counter change");
+  }, [keyword, counter])
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div >
   );
 }
-/*앱을 실행시키고 HTML을 열어보면 h1과 button의 className은 랜덤하게 설정되어있다는 것을 알 수 있음
-=> 각자 다른 컴포넌트에서 같은 className을 쓰게 되더라도 걱정할 필요가 없다는 뜻(각각의 className을 안겹치기 위해 매번 기억하지 않아도 됨)
-*/
-
-Button.PropTypes = {
-  text: PropTypes.string.isRequired,
-}
 
 export default App;
+/*API를 외부에서 가져오는 코드를 작성했을 때 렌더링 할때마다 가져오는 작업을 막기 위해,
+input태그의 onChange속성에 함수를 대입해 값을 입력할 때마다 렌더링이 되기 때문에 => useEffect 사용
+useEffect는 2개의 argument를 갖는데
+1. 첫번째는 우리가 딱 한번만 실행시키고 싶은 코드
+2. 두번째는 [변수]가 변화할 때마다 useEffect를 실행시키고 싶다고 알려주는 것
+useEffect내의 코드는 처음 렌더링 후에 실행됨(나중에 코드 꼬이지 않으려면 알고 있어야 됨)
+
+input태그는 값이 변할 때 onChange상태가 되고 그 상태의 event가 입력한 text를 가지고 있음(input - onChange(event.target.value))
+*/
