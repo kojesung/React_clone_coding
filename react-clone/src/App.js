ae35e8ab1d75f4351a230fd8737ab5d533b46a73
kojesung <js95112345@gmail.com>
2023-12-30 17:59:16 +0900

    To Do List(1) 전개 연산자 활용

--- react-clone/src/App.js
+++ react-clone/src/App.js
@@ -1,21 +1,31 @@
-/*import Button from "./Button";*/
-import PropTypes from "prop-types";
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
-function Hello() {
-  useEffect(() => {
-    console.log("created");
-    return () => console.log("destroyed")
-  }, [])
-  return <h1>Hello</h1>
-}
+
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([])
+  const handleOnChange = (event) => { setToDo(event.target.value) }
+  const handleOnSubmit = (event) => {
+    event.preventDefault();//사용자의 폼 제출을 막아주는 함수
+    console.log(toDo)
+    if (toDo === "") {
+      return
+    }
+    else {
+      setToDo("")
+    }
+    setToDos(function (currentArray) {
+      const newArray = [...currentArray, toDo]
+      console.log(newArray)
+      return newArray
+    })
+  }
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
+      <h1>My To Do List({(toDos.length)})</h1>
+      <form onSubmit={handleOnSubmit}>
+        <input value={toDo} onChange={handleOnChange} type='text' placeholder="Write your to do..." />
+        <button>Add to do</button>
+      </form>
     </div >
   );
 }
@@ -27,8 +37,9 @@ useEffect는 2개의 argument를 갖는데
 1. 첫번째는 우리가 딱 한번만 실행시키고 싶은 코드
 2. 두번째는 [변수]가 변화할 때마다 useEffect를 실행시키고 싶다고 알려주는 것
 useEffect내의 코드는 처음 렌더링 후에 실행됨(나중에 코드 꼬이지 않으려면 알고 있어야 됨)
-3. 함수 컴포넌트 안에 useEffect를 실행시켜주면 컴포넌트가 호출될 때 useEffect가 실행됨,
-해당 컴포넌트가 없어질 때도 호출하고 싶을 땐 function을 return 해주면 됨
+3. 함수 컴포넌트 안에 useEffect를 실행시켜주면 컴포넌트가 호출될 때 useEffect가 실행됨(함수가 작동함),
+해당 컴포넌트가 없어질 때도 호출하고 싶을 땐(다른 함수를 작동 시키고 싶을 때) function을 return 해주면 됨
+함수, 컴포넌트가 없어질 때의 어떤 분석 결과를 보내고 싶을 떄 활용
 
 input태그는 값이 변할 때 onChange상태가 되고 그 상태의 event가 입력한 text를 가지고 있음(input - onChange(event.target.value))
 */
